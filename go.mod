574bb916ef091dcbb9aced9167b0586d4e960919
Sendu Bala <sb10@sanger.ac.uk>
2023-09-15 10:11:20 +0100

    New S3 package.

--- go.mod
+++ go.mod
@@ -5,8 +5,25 @@ go 1.20
 require github.com/smartystreets/goconvey v1.8.1
 
 require (
+	github.com/VertebrateResequencing/muxfys v3.0.5+incompatible // indirect
+	github.com/alexflint/go-filemutex v1.2.0 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/hanwen/go-fuse v1.0.0 // indirect
+	github.com/inconshreveable/log15 v2.16.0+incompatible // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/minio/minio-go v6.0.14+incompatible // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/sb10/l15h v0.0.0-20170510122137-64c488bf8e22 // indirect
 	github.com/smarty/assertions v1.15.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ s3/s3_test.go
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Genome Research Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package s3
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestS3(t *testing.T) {
+	s3bucketPath := os.Getenv("GSB_S3_TEST_PATH")
+	if s3bucketPath == "" {
+		SkipConvey("skipping S3 tests since GSB_S3_TEST_PATH not set", t, func() {})
+		return
+	}
+
+	basePath := strings.Join(strings.Split(s3bucketPath[1:], "/")[1:], "/")
+
+	Convey("Given a New S3 object that uses config on disk", t, func() {
+		s3, err := New(s3bucketPath)
+		So(err, ShouldBeNil)
+		So(s3, ShouldNotBeNil)
+
+		Convey("You can upload a string as a file", func() {
+			basename := "test.txt"
+			testData := "test"
+
+			err = s3.UploadData(strings.NewReader(testData), basename)
+			So(err, ShouldBeNil)
+
+			defer s3.DeleteFile(basePath + "/" + basename)
+
+			entries, err := s3.ListEntries(basePath + "/")
+			So(err, ShouldBeNil)
+			So(len(entries), ShouldEqual, 1)
+			So(entries[0].Name, ShouldEqual, basePath+"/"+basename)
+
+			Convey("And then download it", func() {
+				tmpDir := t.TempDir()
+				tmpFile := filepath.Join(tmpDir, basename)
+
+				err := s3.DownloadFile(basename, tmpFile)
+				So(err, ShouldBeNil)
+
+				f, err := os.Open(tmpFile)
+				So(err, ShouldBeNil)
+
+				defer f.Close()
+
+				buf, err := io.ReadAll(f)
+				So(err, ShouldBeNil)
+				So(string(buf), ShouldEqual, testData)
+			})
+		})
+	})
+}
