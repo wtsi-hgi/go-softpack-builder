4bd0e140457451e42bc04ce1380c9a9df7e73669
Michael Woolnough <mw31@sanger.ac.uk>
2023-11-06 15:44:05 +0000

    Add package to remove existing environments.

---
+++ remove/remove.go
@@ -0,0 +1,140 @@
+package remove
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/wtsi-hgi/go-softpack-builder/build"
+	"github.com/wtsi-hgi/go-softpack-builder/config"
+	"golang.org/x/sys/unix"
+)
+
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+type coreResponse struct {
+	Message string `json:"message,omitempty"`
+	Path    string `json:"path,omitempty"`
+	Name    string `json:"name,omitempty"`
+}
+
+const graphQLDeleteEnvironment = `mutation ($name: String!, $envPath: String!) {
+	deleteEnvironment(
+			name: $name
+			path: $envPath
+	) {
+			... on DeleteEnvironmentSuccess {
+					message
+			}
+			... on EnvironmentNotFoundError {
+					message
+					path
+					name
+			}
+	}
+}`
+
+const graphQLEndpoint = "/graphql"
+
+type graphQLDeleteEnvironmentMutation struct {
+	Query     string `json:"query"` //graphQLDeleteEnvironment
+	Variables struct {
+		Name    string `json:"name"`
+		EnvPath string `json:"envPath"`
+	} `json:"variables"`
+}
+
+func Remove(conf *config.Config, envPath, version string) error {
+	envPath = filepath.Clean(string(filepath.Separator) + envPath)[1:]
+
+	modulePath := filepath.Join(conf.Module.ModuleInstallDir, envPath)
+	scriptPath := filepath.Join(conf.Module.ScriptsInstallDir, envPath, version+build.ScriptsDirSuffix)
+
+	if err := checkWriteAccess(modulePath, scriptPath); err != nil {
+		return err
+	}
+
+	if err := removeEnvFromCore(conf, envPath); err != nil {
+		return err
+	}
+
+	if err := removeLocalFiles(modulePath, scriptPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkWriteAccess(modulePath, scriptPath string) error {
+	for _, p := range [...]string{
+		filepath.Dir(modulePath),
+		modulePath,
+		filepath.Dir(filepath.Dir(scriptPath)),
+		filepath.Dir(scriptPath),
+		scriptPath,
+	} {
+		if err := unix.Access(p, unix.W_OK); err != nil {
+			return Error(fmt.Sprintf("no write access to dir (%s): %s", p, err))
+		}
+	}
+
+	return nil
+}
+
+func removeEnvFromCore(conf *config.Config, envPath string) error {
+	mutation := graphQLDeleteEnvironmentMutation{
+		Query: graphQLDeleteEnvironment,
+	}
+
+	mutation.Variables.Name = filepath.Base(envPath)
+	mutation.Variables.EnvPath = filepath.Dir(envPath)
+
+	var buf bytes.Buffer
+
+	json.NewEncoder(&buf).Encode(mutation)
+
+	req, err := http.NewRequest(http.MethodPost, conf.CoreURL+graphQLEndpoint, &buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var cr coreResponse
+
+	err = json.NewDecoder(resp.Body).Decode(&cr)
+	if err != nil {
+		return err
+	}
+
+	if cr.Name != "" {
+		return Error(cr.Message)
+	}
+
+	return nil
+}
+
+func removeLocalFiles(modulePath, scriptPath string) error {
+	err := os.RemoveAll(modulePath)
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll(scriptPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ remove/remove_test.go
@@ -0,0 +1,160 @@
+package remove
+
+import (
+	"encoding/json"
+	"io"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/wtsi-hgi/go-softpack-builder/build"
+	"github.com/wtsi-hgi/go-softpack-builder/config"
+)
+
+const (
+	groupsDir        = "groups"
+	singularityImage = "singularity.sif"
+)
+
+func TestRemove(t *testing.T) {
+	Convey("With a valid config and a test environment to be removed", t, func() {
+		conf, group, env, version := createTestEnv(t)
+
+		envPath := filepath.Join(groupsDir, group, env)
+
+		var response coreResponse
+
+		core := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			json.NewEncoder(w).Encode(response)
+		}))
+
+		conf.CoreURL = core.URL
+
+		Convey("Remove() call fails if the environments module dir or script dir is not removable", func() {
+			for _, p := range [...]string{
+				filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group),
+				filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env),
+				filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group),
+				filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group, env),
+				filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group, env, version+build.ScriptsDirSuffix),
+			} {
+				err := os.Chmod(p, 0)
+				So(err, ShouldBeNil)
+
+				err = Remove(conf, envPath, version)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "no write access to dir ("+p+"): permission denied")
+
+				err = os.Chmod(p, 0755)
+				So(err, ShouldBeNil)
+			}
+
+			_, err := os.Stat(filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env, version))
+			So(err, ShouldBeNil)
+
+			_, err = os.Stat(filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group, env, version+build.ScriptsDirSuffix, singularityImage))
+			So(err, ShouldBeNil)
+
+			removing := filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env)
+
+			err = os.RemoveAll(removing)
+			So(err, ShouldBeNil)
+
+			err = Remove(conf, envPath, version)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "no write access to dir ("+removing+"): no such file or directory")
+
+			_, err = os.Stat(filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env, version))
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Remove() call fails if environment is not successfully removed from Core", func() {
+			response.Message = "No environment with this name found in this location."
+			response.Path = filepath.Join(groupsDir, group)
+			response.Name = env + "-" + version
+
+			err := Remove(conf, envPath, version)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "No environment with this name found in this location.")
+
+			conf.CoreURL = "http://invalid-url:1234/"
+
+			err = Remove(conf, envPath, version)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Can use Remove() to delete an existing environment", func() {
+			response.Message = "Successfully deleted the environment"
+
+			modulePath := filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env)
+			scriptsPath := filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group, env, version+build.ScriptsDirSuffix)
+
+			err := Remove(conf, envPath, version)
+			So(err, ShouldBeNil)
+
+			_, err = os.Stat(modulePath)
+			So(err, ShouldWrap, os.ErrNotExist)
+
+			_, err = os.Stat(scriptsPath)
+			So(err, ShouldWrap, os.ErrNotExist)
+		})
+	})
+}
+
+func createTestEnv(t *testing.T) (*config.Config, string, string, string) {
+	conf := new(config.Config)
+	conf.Module.ModuleInstallDir = t.TempDir()
+	conf.Module.ScriptsInstallDir = t.TempDir()
+
+	group := genRandString(5)
+	env := genRandString(8)
+	version := genRandString(3)
+
+	modulePath := filepath.Join(conf.Module.ModuleInstallDir, groupsDir, group, env)
+	scriptsPath := filepath.Join(conf.Module.ScriptsInstallDir, groupsDir, group, env, version+build.ScriptsDirSuffix)
+
+	err := os.MkdirAll(modulePath, 0755)
+	So(err, ShouldBeNil)
+
+	err = os.MkdirAll(scriptsPath, 0755)
+	So(err, ShouldBeNil)
+
+	f, err := os.Create(filepath.Join(modulePath, version))
+	So(err, ShouldBeNil)
+
+	_, err = io.WriteString(f, "A Module File")
+	So(err, ShouldBeNil)
+	So(f.Close(), ShouldBeNil)
+
+	f, err = os.Create(filepath.Join(scriptsPath, singularityImage))
+	So(err, ShouldBeNil)
+
+	_, err = io.WriteString(f, "An Image File")
+	So(err, ShouldBeNil)
+	So(f.Close(), ShouldBeNil)
+
+	return conf, group, env, version
+}
+
+func genRandString(length int) string {
+	var sb strings.Builder
+
+	sb.Grow(length)
+
+	for ; length > 0; length-- {
+		letter := rand.Intn(52)
+
+		if letter < 26 {
+			sb.WriteByte(byte(letter) + 'A')
+		} else {
+			sb.WriteByte(byte(letter) - 26 + 'a')
+		}
+	}
+
+	return sb.String()
+}
